15d0cb7c850f1146354a4f30a1a2e06d4de0299f
Serdar Ulutas <ulut0002@algonquinlive.com>
2023-10-13 13:09:34 -0400

    Initial Commit

---
+++ Week6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week6
+//
+//  Created by Serdar Ulutas on 2023-10-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Week6/Week6App.swift
@@ -0,0 +1,17 @@
+//
+//  Week6App.swift
+//  Week6
+//
+//  Created by Serdar Ulutas on 2023-10-13.
+//
+
+import SwiftUI
+
+@main
+struct Week6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
